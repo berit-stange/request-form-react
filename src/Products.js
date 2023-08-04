c91f0ef1f3ba91e9adfb22a72df707b447342808
berit-stange <76659950+berit-stange@users.noreply.github.com>
2023-08-04 14:54:52 +0200

    changed "a" to "Link"

--- src/Products.js
+++ src/Products.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './css/styles.css';
 import './css/background-images.css';
 import { Link } from 'react-router-dom';
@@ -10,79 +11,74 @@ function Products() {
             <div className="product-grid">
 
                 <Link to="/printareastool">
-                    {/* <div className="product-item hocker-maks"></div> */}
+                    <div className="product-item hocker-maks"></div>
                     <p className="product-title">Hocker MAKS</p>
                 </Link>
 
-                {/* <a href="#" className="">
-                    <div className="product-item hocker-maks"></div>
-                    <p className="product-title">Hocker MAKS</p>
-                </a> */}
-
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item hocker-sitzwuerfel"></div>
                     <p className="product-title">Hocker SITZWÜRFEL</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item hocker-barhocker"> </div>
                     <p className="product-title">Hocker BARHOCKER</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-dino"> </div>
                     <p className="product-title">Stellwand DINO</p>
-                </a>
+                </Link>
 
-                {/* <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-muro-100x200"></div>
                     <p className="product-title">Stellwand MURO 100 x 200 cm</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-muro-110x200"></div>
                     <p className="product-title">Stellwand MURO 110 x 200 cm</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-muro-120x200"></div>
                     <p className="product-title">Stellwand MURO 120 x 200 cm</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-muro-100x220"></div>
                     <p className="product-title">Stellwand MURO 100 x 220 cm</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-muro-110x220"></div>
                     <p className="product-title">Stellwand MURO 110 x 220 cm</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-muro-120x220"></div>
                     <p className="product-title">Stellwand MURO 120 x 220 cm</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-pin-point"></div>
                     <p className="product-title">Stellwand PIN POINT</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-pin-point-xl"></div>
                     <p className="product-title">Stellwand PIN POINT XL</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-quick-point-h"></div>
                     <p className="product-title">Stellwand QUICK POINT Hochformat</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/printareastool">
                     <div className="product-item stellwand-quick-point-q"></div>
                     <p className="product-title">Stellwand QUICK POINT Querformat</p>
-                </a> */}
+                </Link>
 
             </div>
 
