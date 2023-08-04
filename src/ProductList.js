c7ab986c772989bdec8a260170ee5532a511a365
berit-stange <76659950+berit-stange@users.noreply.github.com>
2023-08-04 14:54:18 +0200

    changed class name and link

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { useState, useEffect, useRef } from "react";
+import './css/styles.css';
+import './css/background-images.css';
+import { Link } from 'react-router-dom';
 
 
 const ProductList = () => {
@@ -14,13 +17,11 @@ const ProductList = () => {
 
     return (
 
-        <div className="">
+        <div className="product-grid">
 
             <h2>xxxx</h2>
 
             <div className="">
-                <h2>xxxx</h2>
-
                 <div className="search-box">
                     <input
                         type="search-input"
@@ -28,8 +29,12 @@ const ProductList = () => {
                         // onChange={(event) => { setSearchTerm(event.target.value); }}
                         aria-label="Suche" />
                 </div>
-
             </div>
+
+            <Link to="/printareastool">
+                {/* <div className="product-item hocker-maks"></div> */}
+                <p className="product-title">Hocker MAKS</p>
+            </Link>
 
         </div>
 
