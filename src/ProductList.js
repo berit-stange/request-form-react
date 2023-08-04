c060be28276627176615eb5e3007c5d0d9cd9851
berit-stange <76659950+berit-stange@users.noreply.github.com>
2023-08-04 14:11:20 +0200

    created ProductList component as class component

---
+++ src/ProductList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState, useEffect, useRef } from "react";
+
+
+const ProductList = () => {
+
+    useEffect(() => {
+
+    }, [
+
+    ]);
+
+
+
+    return (
+
+        <div className="">
+
+            <h2>xxxx</h2>
+
+            <div className="">
+                <h2>xxxx</h2>
+
+                <div className="search-box">
+                    <input
+                        type="search-input"
+                        placeholder="Suche"
+                        // onChange={(event) => { setSearchTerm(event.target.value); }}
+                        aria-label="Suche" />
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+}
+
+export default ProductList;
