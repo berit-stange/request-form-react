21f20258877d028043dd346bbbff7ba1ec5de821
Berit <berit_stange@gmx.de>
2023-08-07 15:48:43 +0200

    prepared progress bar component for later

--- src/Progress.js
+++ src/Progress.js
@@ -1,10 +1,24 @@
 import './css/styles.css';
+import { BrowserRouter as Router, Route, Redirect, BrowserHistory, useNavigate } from "react-router-dom"; // for goBack() ? 
+
 
 function Progress() {
+
+
+
   return (
     <div className="progress">
-      
-   
+
+      <div className='line'></div>
+
+      <div className='button-box'>
+        <button className="button-progress" onClick={() => { }} >1</button>
+        <button className="button-progress" onClick={() => { }} >2</button>
+        <button className="button-progress" onClick={() => { }} >3</button>
+        <button className="button-progress" onClick={() => { }} >4</button>
+        <button className="button-progress" onClick={() => { }} >5</button>
+        <button className="button-progress" onClick={() => { }} >6</button>
+      </div>
 
     </div>
   );
