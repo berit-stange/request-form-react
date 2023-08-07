d9a27258abde6277c8852497c248c04f8e732174
Berit <berit_stange@gmx.de>
2023-08-07 10:19:12 +0200

    created component for Stellwand MURO

---
+++ src/PrintareaStellwandMuro.js
@@ -0,0 +1,36 @@
+import './css/styles.css';
+import './css/background-images.css';
+
+function PrintareaStellwandMuro() {
+    return (
+        <div className="products">
+
+            <div className="product-grid">
+
+                <a href="#" className="">
+                    <div className="product-item stellwand-muro-1"></div>
+                    <p className="product-title">1 Seite / Mittelwand</p>
+                </a>
+
+                <a href="#">
+                    <div className="product-item stellwand-muro-1-b"></div>
+                    <p className="product-title">1 Seite mit schmaler Seite</p>
+                </a>
+
+                <a href="#" className="">
+                    <div className="product-item stellwand-muro-2"></div>
+                    <p className="product-title">2 Seiten / Mittelwand</p>
+                </a>
+
+                <a href="#" className="">
+                    <div className="product-item stellwand-muro-2-b"></div>
+                    <p className="product-title">2 Seiten mit schmalen Seiten</p>
+                </a>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default PrintareaStellwandMuro; 
