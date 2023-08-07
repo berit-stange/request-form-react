a3077cec066fbb9ebb8c8e81393e1262a009d292
Berit <berit_stange@gmx.de>
2023-08-07 09:39:28 +0200

    created component for Stellwand DINO options

---
+++ src/PrintareaStellwandDino.js
@@ -0,0 +1,31 @@
+import './css/styles.css';
+import './css/background-images.css';
+
+function PrintareaStellwandDino() {
+    return (
+        <div className="products">
+
+            <div className="product-grid">
+
+                <a href="#" className="">
+                    <div className="product-item stellwand-dino-1"></div>
+                    <p className="product-title">1 Seite</p>
+                </a>
+
+                <a href="#">
+                    <div className="product-item stellwand-dino-2"></div>
+                    <p className="product-title">2 Seiten</p>
+                </a>
+
+                <a href="#">
+                    <div className="product-item stellwand-dino-3"></div>
+                    <p className="product-title">komplett bedruckt</p>
+                </a>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default PrintareaStellwandDino; 
