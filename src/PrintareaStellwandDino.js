f33eeb8904f91763ace5f472cd9b1ae37b3dfb53
Berit <berit_stange@gmx.de>
2023-08-07 09:52:56 +0200

    added options and classes

--- src/PrintareaStellwandDino.js
+++ src/PrintareaStellwandDino.js
@@ -9,17 +9,22 @@ function PrintareaStellwandDino() {
 
                 <a href="#" className="">
                     <div className="product-item stellwand-dino-1"></div>
-                    <p className="product-title">1 Seite</p>
+                    <p className="product-title">1 Seite / Mittelwand</p>
                 </a>
 
                 <a href="#">
+                    <div className="product-item stellwand-dino-1-b"></div>
+                    <p className="product-title">1 Seite mit Stützbeinen</p>
+                </a>
+
+                <a href="#" className="">
                     <div className="product-item stellwand-dino-2"></div>
-                    <p className="product-title">2 Seiten</p>
+                    <p className="product-title">2 Seiten / Mittelwand</p>
                 </a>
 
-                <a href="#">
-                    <div className="product-item stellwand-dino-3"></div>
-                    <p className="product-title">komplett bedruckt</p>
+                <a href="#" className="">
+                    <div className="product-item stellwand-dino-2-b"></div>
+                    <p className="product-title">2 Seiten mit Stützbeinen</p>
                 </a>
 
             </div>
