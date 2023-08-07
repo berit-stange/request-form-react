f629e29001989f257ca399c0dc2ddb668e00ffc2
Berit <berit_stange@gmx.de>
2023-08-07 17:23:35 +0200

    added route for PIN POINT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './stange-design-logo.svg';
+// import logo from './stange-design-logo.svg';
 // import './App.css';
 import { BrowserRouter, Routes, Route, BrowserHistory } from 'react-router-dom';
 import PrintareaStool from './PrintareaStool';
@@ -7,6 +7,7 @@ import PrintareaSitzwuerfel from './PrintareaSitzwuerfel';
 import PrintareaBarhocker from './PrintareaBarhocker';
 import PrintareaStellwandDino from './PrintareaStellwandDino';
 import PrintareaStellwandMuro from './PrintareaStellwandMuro';
+import PrintareaStellwandPinPoint from './PrintareaStellwandPinPoint';
 
 import './css/styles.css';
 import Header from './Header';
@@ -27,13 +28,14 @@ function App() {
       {/* <Progress  /> */}
 
       <Routes >
-        <Route exact path="/" element={<Products />} />
-        <Route exact path="/productlist" element={<ProductList />} />
-        <Route exact path="/printareastool" element={<PrintareaStool />} />
-        <Route exact path="/printareasitzwuerfel" element={<PrintareaSitzwuerfel />} />
-        <Route exact path="/printarea-barhocker" element={<PrintareaBarhocker />} />
-        <Route exact path="/printarea-stellwand-dino" element={<PrintareaStellwandDino />} />
-        <Route exact path="/printarea-stellwand-muro" element={<PrintareaStellwandMuro />} />
+        <Route exact path="/anfrage/" element={<Products />} />
+        <Route exact path="/anfrage/productlist" element={<ProductList />} />
+        <Route exact path="/anfrage/printarea-stool-maks" element={<PrintareaStool />} />
+        <Route exact path="/anfrage/printareasitzwuerfel" element={<PrintareaSitzwuerfel />} />
+        <Route exact path="/anfrage/printarea-barhocker" element={<PrintareaBarhocker />} />
+        <Route exact path="/anfrage/printarea-stellwand-dino" element={<PrintareaStellwandDino />} />
+        <Route exact path="/anfrage/printarea-stellwand-muro" element={<PrintareaStellwandMuro />} />
+        <Route exact path="/anfrage/printarea-stellwand-pinpoint" element={<PrintareaStellwandPinPoint />} />
       </Routes>
 
       {/* </div> */}
