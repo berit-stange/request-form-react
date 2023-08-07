7b45eea9da9fd3588dd41d4a0c49fe6219a83586
Berit <berit_stange@gmx.de>
2023-08-07 15:57:27 +0200

    added link to header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,17 @@
 import logo from './stange-design-logo.svg';
 
 import './css/styles.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header className="">
 
+      <Link to="/">
         <img src={logo} className="logo" alt="logo" />
+      </Link>
+
+
 
     </header>
   );
