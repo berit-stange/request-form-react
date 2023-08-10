dd36a1a0946b2994cc54646ef2b1177e39b182a1
Berit <berit_stange@gmx.de>
2023-08-10 14:05:57 +0200

    added link to color option component

--- src/PrintareaSitzwuerfel.js
+++ src/PrintareaSitzwuerfel.js
@@ -19,25 +19,25 @@ function PrintareaSitzwuerfel() {
 
             <div className="product-grid">
 
-                <a href="#" className="">
+                <Link to="/anfrage/color-options">
                     <div className="product-item hocker-sitzwuerfel-1"></div>
                     <p className="product-title">1 Seite mit Logo bis max. A4</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/anfrage/color-options">
                     <div className="product-item hocker-sitzwuerfel-2"></div>
                     <p className="product-title">2 Seiten mit Logo bis max. A4</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/anfrage/color-options">
                     <div className="product-item hocker-sitzwuerfel-4"></div>
                     <p className="product-title">4 Seiten mit Logo bis max. A4</p>
-                </a>
+                </Link>
 
-                <a href="#">
+                <Link to="/anfrage/color-options">
                     <div className="product-item hocker-sitzwuerfel-komplett"></div>
                     <p className="product-title">komplett bedruckt</p>
-                </a>
+                </Link>
 
             </div>
 
