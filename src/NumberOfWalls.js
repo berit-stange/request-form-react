40e77ff0704dd17349c5b48239bff000673e1acb
Berit <berit_stange@gmx.de>
2023-08-15 16:56:29 +0200

    created component to choose number of walls

---
+++ src/NumberOfWalls.js
@@ -0,0 +1,35 @@
+import './css/styles.css';
+import './css/background-images.css';
+import { Link } from 'react-router-dom';
+
+function NumberOfWalls() {
+    return (
+        <div className="products">
+
+            <div className="progress">
+                <div className='line'></div>
+                <div className='button-box'>
+                    <button className="button-progress active" onClick={() => { }} >1</button>
+                    <button className="button-progress active" onClick={() => { }} >2</button>
+                    <button className="button-progress active" onClick={() => { }} >3</button>
+                    <button className="button-progress active" onClick={() => { }} >4</button>
+                    <button className="button-progress" onClick={() => { }} >5</button>
+                    <button className="button-progress" onClick={() => { }} >6</button>
+                </div>
+            </div>
+
+            <div>
+                <p className="erklaerung">Wählen Sie die Farben aus. Je nach Stückzahl und Produkt entscheiden wir,
+                    wie produziert wird.</p>
+            </div>
+
+            <form className="number-input-box">
+                <input className="number-input"></input>
+            </form>
+
+
+        </div>
+    );
+}
+
+export default NumberOfWalls;
