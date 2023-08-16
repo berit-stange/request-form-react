21a708e26127ff750642ed5e1ba97055b50f34d7
Berit <berit_stange@gmx.de>
2023-08-16 12:12:14 +0200

    created component for color option for walls

---
+++ src/ColorOptionsWall.js
@@ -0,0 +1,49 @@
+import './css/styles.css';
+import './css/background-images.css';
+import { Link } from 'react-router-dom';
+
+function ColorOptionsWall() {
+    return (
+        <div className="products">
+
+            <div className="progress">
+                <div className='line'></div>
+                <div className='button-box'>
+                    <button className="button-progress active" onClick={() => { }} >1</button>
+                    <button className="button-progress active" onClick={() => { }} >2</button>
+                    <button className="button-progress active" onClick={() => { }} >3</button>
+                    <button className="button-progress" onClick={() => { }} >4</button>
+                    <button className="button-progress" onClick={() => { }} >5</button>
+                    <button className="button-progress" onClick={() => { }} >6</button>
+                </div>
+            </div>
+
+            <div>
+                <p className="erklaerung">Wählen Sie die Farben aus. Je nach Stückzahl und Farbigkeit entscheiden wir,
+                    ob mit Sieb- oder Digitaldruck produziert wird.</p>
+            </div>
+
+            <div className="product-grid">
+
+                <Link to="/anfrage/number-of-stools">
+                    <div className="product-item weiss-auf-natur"></div>
+                    <p className="product-title">Weiß auf naturbraun</p>
+                </Link>
+
+                <Link to="/anfrage/number-of-stools">
+                    <div className="product-item schwarz-auf-natur"></div>
+                    <p className="product-title">Schwarz auf naturbraun</p>
+                </Link>
+
+                <Link to="/anfrage/number-of-stools">
+                    <div className="product-item farbe-auf-weiss-maks-1"></div>
+                    <p className="product-title">1 Farbe auf Weiss</p>
+                </Link>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default ColorOptionsWall;
