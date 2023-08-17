06aeef62905847503a3a6f2b027a1382ecbc3c0a
Berit <berit_stange@gmx.de>
2023-08-17 15:22:25 +0200

    created component for Quick Point Hochformat

---
+++ src/PrintareaStellwandQuickPointHoch.js
@@ -0,0 +1,53 @@
+import './css/styles.css';
+import './css/background-images.css';
+import { Link } from 'react-router-dom';
+
+function PrintareaStellwandQuickPointHoch() {
+    return (
+        <div className="products">
+
+            <div className="progress">
+                <div className='line'></div>
+                <div className='button-box'>
+                    <button className="button-progress active" onClick={() => { }} >1</button>
+                    <button className="button-progress active" onClick={() => { }} >2</button>
+                    <button className="button-progress" onClick={() => { }} >3</button>
+                    <button className="button-progress" onClick={() => { }} >4</button>
+                    <button className="button-progress" onClick={() => { }} >5</button>
+                    <button className="button-progress" onClick={() => { }} >6</button>
+                </div>
+            </div>
+
+            <div>
+                <p className="erklaerung">Die Größe der Druckfläche ist der wichtigste Faktor für den Preis.</p>
+            </div>
+
+            <div className="product-grid">
+
+                <Link to="/anfrage/color-options-wall">
+                    <div className="product-item stellwand-quick-point-1"></div>
+                    <p className="product-title">1 Seite / Mittelwand ohne Stützbeine</p>
+                </Link>
+
+                <Link to="/anfrage/color-options-wall">
+                    <div className="product-item stellwand-quick-point-1-b"></div>
+                    <p className="product-title">1 Seite mit Stützbeinen</p>
+                </Link>
+
+                <Link to="/anfrage/color-options-wall">
+                    <div className="product-item stellwand-quick-point-2"></div>
+                    <p className="product-title">2 Seiten / Mittelwand ohne Stützbeine</p>
+                </Link>
+
+                <Link to="/anfrage/color-options-wall">
+                    <div className="product-item stellwand-quick-point-2-b"></div>
+                    <p className="product-title">2 Seiten mit Stützbeinen</p>
+                </Link>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default PrintareaStellwandQuickPointHoch; 
