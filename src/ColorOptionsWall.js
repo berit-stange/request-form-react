404a1f45f75fde836b3d581d377bc38a2a5b9133
Berit <berit_stange@gmx.de>
2023-08-22 13:30:43 +0200

    corrected links

--- src/ColorOptionsWall.js
+++ src/ColorOptionsWall.js
@@ -20,22 +20,22 @@ function ColorOptionsWall() {
 
             <div>
                 <p className="erklaerung">Wählen Sie die Farben aus. Je nach Stückzahl und Farbigkeit entscheiden wir,
-                    ob mit Sieb- oder Digitaldruck produziert wird.</p>
+                    wie produziert wird.</p>
             </div>
 
             <div className="product-grid">
 
-                <Link to="/anfrage/number-of-stools">
+                <Link to="/anfrage/number-of-walls">
                     <div className="product-item weiss-auf-natur"></div>
                     <p className="product-title">Weiß auf naturbraun</p>
                 </Link>
 
-                <Link to="/anfrage/number-of-stools">
+                <Link to="/anfrage/number-of-walls">
                     <div className="product-item schwarz-auf-natur"></div>
                     <p className="product-title">Schwarz auf naturbraun</p>
                 </Link>
 
-                <Link to="/anfrage/number-of-stools">
+                <Link to="/anfrage/number-of-walls">
                     <div className="product-item komplett-farbig-wand-muro"></div>
                     <p className="product-title">Vollfarbig</p>
                 </Link>
